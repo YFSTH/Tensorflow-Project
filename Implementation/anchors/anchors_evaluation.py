c5a9541121568c95b3ef27c768de29a147decbf7
YFSTH <yan.thorsting@googlemail.com>
2018-02-24 15:53:36 +0100

    the uprising of the anchors

--- Implementation/anchors/anchors_evaluation.py
+++ Implementation/anchors/anchors_evaluation.py
@@ -1,7 +1,7 @@
 import pdb
 from anchors.debugging_module import *
 
-def anchors_evaluation(batch_anchor_tensor, imgs, labels):
+def anchors_evaluation(batch_anchor_tensor, imgs, labels, load_last_anchors, filename):
     '''
     This function discriminates between positive, negative and neutral anchors and additionally
     assigns for each image to each positive anchor the optimal ground truth box (if such a box exists)
@@ -19,66 +19,81 @@ def anchors_evaluation(batch_anchor_tensor, imgs, labels):
     from anchors.Anchor import Anchor
     from anchors.GroundTruthBox import GroundTruthBox
     from anchors.create_ground_truth_tensor_and_selection_tensor import create_ground_truth_tensor_and_selection_tensor
+    import pickle
+    import os
 
-    num_imgs = len(labels)
-    fm_w_index, fm_h_index = batch_anchor_tensor.shape[-2], batch_anchor_tensor.shape[-1]
-    num_anchors = batch_anchor_tensor.shape[1] // 4
-    anchor_tensor = batch_anchor_tensor[0, :, :, :]
-    # is of shape (NUM_ANCHORS*4, feature map width, feature map height)
+    just_reload = False
+    if load_last_anchors:
+        # Check whether collages already exist if that is the case do nothing
+        files = os.listdir('./anchors')
+        for f in files:
+            if filename+'.pkl' in f:
+                just_reload = True
 
-    ground_truth_tensors = []
-    selection_tensors = []
-    for collage in range(num_imgs):
+    if just_reload is not True:
+        num_imgs = len(labels)
+        fm_w_index, fm_h_index = batch_anchor_tensor.shape[-2], batch_anchor_tensor.shape[-1]
+        num_anchors = batch_anchor_tensor.shape[1] // 4
+        anchor_tensor = batch_anchor_tensor[0, :, :, :]
+        # is of shape (NUM_ANCHORS*4, feature map width, feature map height)
 
-        # Create list of anchor objects
-        anchor_objects = []
-        for w_idx in range(fm_w_index):
-            for h_idx in range(fm_h_index):
-                for anchor_index in range(num_anchors):
-                    x = anchor_tensor[anchor_index, w_idx, h_idx]
-                    y = anchor_tensor[anchor_index+num_anchors, w_idx, h_idx]
-                    w = anchor_tensor[anchor_index+num_anchors*2, w_idx, h_idx]
-                    h = anchor_tensor[anchor_index+num_anchors*3, w_idx, h_idx]
-                    anchor_objects.append(Anchor(x, y, w, h, w_idx, h_idx, anchor_index))
+        ground_truth_tensors = []
+        selection_tensors = []
 
-        # (get labels of specific collage)
-        mnist_imgs = labels[collage]
-        # for each mnist image in the collage create one ground truth box
-        ground_truth_boxes = []
-        for mnist_image in enumerate(mnist_imgs):
-            if mnist_image[1][0] >= 0: # filter out filler (-9,...,-9) labels
-                x = mnist_image[1][1]
-                y = mnist_image[1][2]
-                w = mnist_image[1][3]
-                h = mnist_image[1][4]
-                label = mnist_image[1][0]
-                ground_truth_boxes.append(GroundTruthBox(x, y, w, h, label))
+        for collage in range(num_imgs):
 
-        # evaluation (anchor <> ground truth box assignment) takes place inside the Anchor object: object-oriented
-        # approach is used to determine the appropriate ground truth box the anchor should transform to (if there is
-        # any)
-        for anchor in anchor_objects:
-            for box in ground_truth_boxes:
-                anchor.append_ground_truth_box(box)
-                for a in anchor_objects:
-                    a.evaluate_anchor()
+            # Create list of anchor objects
+            anchor_objects = []
+            for w_idx in range(fm_w_index):
+                for h_idx in range(fm_h_index):
+                    for anchor_index in range(num_anchors):
+                        x = anchor_tensor[anchor_index, w_idx, h_idx]
+                        y = anchor_tensor[anchor_index+num_anchors, w_idx, h_idx]
+                        w = anchor_tensor[anchor_index+num_anchors*2, w_idx, h_idx]
+                        h = anchor_tensor[anchor_index+num_anchors*3, w_idx, h_idx]
+                        anchor_objects.append(Anchor(x, y, w, h, w_idx, h_idx, anchor_index))
 
+            # (get labels of specific collage)
+            mnist_imgs = labels[collage]
+            # for each mnist image in the collage create one ground truth box
+            ground_truth_boxes = []
+            for mnist_image in enumerate(mnist_imgs):
+                if mnist_image[1][0] >= 0: # filter out filler (-9,...,-9) labels
+                    x = mnist_image[1][1]
+                    y = mnist_image[1][2]
+                    w = mnist_image[1][3]
+                    h = mnist_image[1][4]
+                    label = mnist_image[1][0]
+                    ground_truth_boxes.append(GroundTruthBox(x, y, w, h, label))
 
-        gtt, slt = create_ground_truth_tensor_and_selection_tensor(anchor_objects, ground_truth_boxes, num_anchors, fm_w_index, fm_h_index)
+            # evaluation (anchor <> ground truth box assignment) takes place inside the Anchor object: object-oriented
+            # approach is used to determine the appropriate ground truth box the anchor should transform to (if there is
+            # any)
+            for anchor in anchor_objects:
+                for box in ground_truth_boxes:
+                    anchor.append_ground_truth_box(box)
+                    for a in anchor_objects:
+                        a.evaluate_anchor()
 
-        # TODO: Plot collages and anchor and ground truth bounding boxes for testing
-        debugging_module(batch_anchor_tensor[collage], imgs[collage], labels[collage], gtt, slt, anchor_objects, ground_truth_boxes)
+            gtt, slt = create_ground_truth_tensor_and_selection_tensor(anchor_objects, ground_truth_boxes, num_anchors, fm_w_index, fm_h_index)
 
-        # create one ground_truth_tensor with shape (NUM_TENSORS*4, feature map width, feature map height) per collage
-        # image
-        ground_truth_tensors.append(gtt)
+            #debugging_module(batch_anchor_tensor[collage], imgs[collage], labels[collage], gtt, slt, anchor_objects, ground_truth_boxes)
 
-        # create one selection tensor with shape (NUM_TENSORS, feature map width, feature map height, [type, class]) per
-        # image, whereas 'type' is the positive xor neutral xor negative evaluation of the tensor and 'class' the mnist
-        # number class
-        selection_tensors.append(slt)
+            # create one ground_truth_tensor with shape (NUM_TENSORS*4, feature map width, feature map height) per collage
+            # image
+            ground_truth_tensors.append(gtt)
 
-        #pdb.set_trace()
+            # create one selection tensor with shape (NUM_TENSORS, feature map width, feature map height, [type, class]) per
+            # image, whereas 'type' is the positive xor neutral xor negative evaluation of the tensor and 'class' the mnist
+            # number class
+            selection_tensors.append(slt)
+
+        with open('anchors/'+filename+'.pkl', 'wb') as file:
+            pickle.dump([ground_truth_tensors, selection_tensors], file)
+
+    if just_reload is True:
+        with open('anchors/'+filename+'.pkl', 'rb') as file:
+            ground_truth_tensors, selection_tensors = pickle.load(file)
 
     return np.array(ground_truth_tensors), np.array(selection_tensors) # should be of shape (NUM_IMAGES, NUM_TENSORS*4, feature map width, feature map height)
 
